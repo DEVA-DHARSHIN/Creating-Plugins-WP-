ec5ad9cc6588a3bb0078ee522aa7de39a7920f18
Devadharshini <143801102+deva16081920@users.noreply.github.com>
2024-08-30 15:47:15 +0530

    Update learning.php

--- learning.php
+++ learning.php
@@ -7,22 +7,28 @@ if(!defined('ABSPATH')){
     exit;
 }
 
-class learning
+class Learning
 {
+    function __construct(){
+        add_action('init',array($this,'cust_post_type'));
+    }
     function activation(){
-        echo 'Hi Im Activated';
+        //echo 'Hi Im Activated';
     }
     function deactivation(){
-        echo 'Hi Im deActivated';
+        //echo 'Hi Im deActivated';
+    }
+    function cust_post_type(){
+        register_post_type('book',['public'=>true,'label'=>'Book']);
     }
 }
 
-if(class_exists('learning')){
-    $Learning = new learning('Hi everyone');
+if(class_exists('Learning')){
+    $learning = new Learning('Hi everyone');
 }
  
-register_activation_hook(__FILE__,array($Learning,'activation'));
-register_deactivation_hook(__FILE__,array($Learning,'deactivation'));
+register_activation_hook(__FILE__,array($learning,'activation'));
+register_deactivation_hook(__FILE__,array($learning,'deactivation'));
 
 
 
