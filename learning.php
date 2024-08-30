c4c0519903835c5999216675a9928a62b7df52e6
Devadharshini <143801102+deva16081920@users.noreply.github.com>
2024-08-30 14:35:43 +0530

    Update learning.php

--- learning.php
+++ learning.php
@@ -6,14 +6,36 @@ Author: Deva
 if(!defined('ABSPATH')){
     exit;
 }
+
 class learning
 {
-    function __construct($args){
-        echo $args;
+    function activation(){
+        echo 'Hi Im Activated';
+    }
+    function deactivation(){
+        echo 'Hi Im deActivated';
     }
 }
 
 if(class_exists('learning')){
     $Learning = new learning('Hi everyone');
 }
+ 
+register_activation_hook(__FILE__,array($Learning,'activation'));
+register_deactivation_hook(__FILE__,array($Learning,'deactivation'));
+
+
+
+//This is a simple class to check with constructor and instance creation.
+// class learning
+// {
+//     function __construct($args){
+//         echo $args;
+//     }
+// }
+
+// if(class_exists('learning')){
+//     $Learning = new learning('Hi everyone');
+// }
+
 ?>  
